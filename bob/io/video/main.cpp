1fb6a68842aa70a67d285c3c2d59c6f750a36331
Manuel Gunther <siebenkopf@googlemail.com>
2015-12-03 13:26:19 -0700

    Corrected error cases in module creation

--- bob/io/video/main.cpp
+++ bob/io/video/main.cpp
@@ -658,11 +658,13 @@ static PyObject* create_module (void) {
 
 # if PY_VERSION_HEX >= 0x03000000
   PyObject* m = PyModule_Create(&module_definition);
+  auto m_ = make_xsafe(m);
+  const char* ret = "O";
 # else
   PyObject* m = Py_InitModule3(BOB_EXT_MODULE_NAME, module_methods, module_docstr);
+  const char* ret = "N";
 # endif
   if (!m) return 0;
-  auto m_ = make_safe(m);
 
   /* register the types to python */
   Py_INCREF(&PyBobIoVideoReader_Type);
@@ -691,7 +693,7 @@ static PyObject* create_module (void) {
     }
   }
 
-  return Py_BuildValue("O", m);
+  return Py_BuildValue(ret, m);
 }
 
 PyMODINIT_FUNC BOB_EXT_ENTRY_NAME (void) {
